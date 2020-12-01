21f33c4b6925b208456fdf2d668c53c98548e5d7
Mariola Rumlova <mariolarumlova@gmail.com>
2020-12-01 08:47:08 +0100

    Added placeholder page for playlist choosing

---
+++ name-that-tune/src/views/ChoosePlaylist.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HomeMenu />
+    <div class="main-container">
+      <div class="loginsuccess-container">
+        <h2 class="heading">Choose playlist</h2>
+        <div>
+          <p>Placeholder</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HomeMenu from "@/components/Menu";
+
+export default {
+  name: "chooseplaylist",
+  components: {
+    HomeMenu
+  }
+};
+</script>

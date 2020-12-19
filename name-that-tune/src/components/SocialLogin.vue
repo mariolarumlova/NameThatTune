25ad3f775fb48d3a7678713b5cd973abdb2d4da2
Mariola Rumlova <mariolarumlova@gmail.com>
2020-12-19 19:12:31 +0100

    [WIP] Authenticating to People API and Youtube API at once

--- name-that-tune/src/components/SocialLogin.vue
+++ name-that-tune/src/components/SocialLogin.vue
@@ -3,7 +3,7 @@
     <v-btn
       color="orange darken-2"
       class="ma-2 white--text"
-      @click="loader = 'loading3'"
+      @click.prevent="logInViaGoogle()"
     >
       Sign in with Google
       <v-icon left color="orange darken-2">mdi-google</v-icon>
@@ -15,48 +15,23 @@
 /* eslint-disable*/
 
 import router from "@/router/router";
-import runSample from "@/middleware/listPlaylist";
+import { getGoogleAuthObject } from "@/middleware/googleApiConnector";
 
 export default {
-  data() {
-    return {
-      /**
-       * The Auth2 parameters, as seen on
-       * https://developers.google.com/identity/sign-in/web/reference#gapiauth2initparams.
-       * As the very least, a valid client_id must present.
-       * @type {Object}
-       */
-      googleSignInParams: {
-        client_id: process.env.VUE_APP_CLIENT_ID,
-      },
-    };
-  },
   methods: {
-    onSignInSuccess(googleUser) {
-      // `googleUser` is the GoogleUser object that represents the just-signed-in user.
-      // See https://developers.google.com/identity/sign-in/web/reference#users
-      console.log(JSON.stringify(googleUser));
-      listPlaylist({accessToken: googleUser.xc.access_token, apiKey: process.env.VUE_APP_YOUTUBE_API_KEY, store: this.$store});
-
-      // Example 1: Use method-specific function
-      // var request = gapi.client.youtube.channels.list({'part': 'snippet', 'mine': 'true'});
-      // request.execute(function(response) {
-      //   console.log(response);
-      // });
-
-      const userInfo = {
-        loginType: "google",
-        google: googleUser,
-      };
-      this.$store.commit("setLoginUser", userInfo);
-      router.push("/choosemode");
+    async logInViaGoogle() {
+      try {
+        const googleAuthObject = await getGoogleAuthObject();
+        const userInfo = {
+          loginType: "google",
+          google: googleUser,
+        };
+        this.$store.commit("setLoginUser", userInfo);
+        router.push("/choosemode");
+      } catch(err) {
+        console.error(err);
+      }
     },
-    onSignInError(error) {
-      console.log("OH NOES", error);
-    },
-    runSample() {
-      runSample();
-    }
   },
 };
 </script>

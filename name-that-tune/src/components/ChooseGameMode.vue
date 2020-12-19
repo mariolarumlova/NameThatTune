5cfd1cdc11b3512d17aa7ec173515931e9c7ed7d
Mariola Rumlova <mariolarumlova@gmail.com>
2020-12-19 23:24:52 +0100

    Authentication to app works, able to perform basic request to youtube api

--- name-that-tune/src/components/ChooseGameMode.vue
+++ name-that-tune/src/components/ChooseGameMode.vue
@@ -12,16 +12,45 @@
           >Tournament</v-btn
         >
       </v-col>
+
+      <v-btn
+        color="orange darken-2"
+        class="ma-2 white--text"
+        @click.prevent="execute()"
+      >
+        Execute
+        <v-icon left color="orange darken-2">mdi-google</v-icon>
+      </v-btn>
     </v-row>
   </v-container>
 </template>
 
 <script>
+/* eslint-disable no-undef */
 export default {
   data() {
     return {
-      clientName: this.$store.state.loginUser.google.wt.Ad
+      clientName: this.$store.state.loginUser.google.wt.Ad,
     };
+  },
+  methods: {
+    execute: function() {
+      return gapi.client.youtube.playlists
+        .list({
+          part: ["snippet,contentDetails"],
+          maxResults: 25,
+          mine: true,
+        })
+        .then(
+          function(response) {
+            // Handle the results here (response.result has the parsed body).
+            console.log("Response", response);
+          },
+          function(err) {
+            console.error("Execute error", err);
+          }
+        );
+    },
   }
 };
 </script>

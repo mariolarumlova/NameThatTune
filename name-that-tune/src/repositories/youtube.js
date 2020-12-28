d74dd143b5726c5c9915ce64002a2f906c2fc1d2
Mariola Rumlova <mariolarumlova@gmail.com>
2020-12-28 18:49:17 +0100

    Youtube repository refactored

--- name-that-tune/src/repositories/youtube.js
+++ name-that-tune/src/repositories/youtube.js
@@ -1,91 +1,54 @@
 import { parse, toSeconds } from "iso8601-duration";
 
-const getVideosWithDurations = (gapi, videos) => {
+const getVideosWithDurations = async (gapi, videos) => {
   const videoIds = videos.map(video => video.id).join(",");
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    gapi.client.youtube.videos
-      .list({
-        part: ["contentDetails,snippet"],
-        id: videoIds
-      })
-      .then(
-        function(response) {
-          console.log("Response - videos", response);
-          const items = response.result.items.map(piece => {
-            const duration = toSeconds(parse(piece.contentDetails.duration));
-            return {
-              id: piece.id,
-              title: piece.snippet.title,
-              avatar: piece.snippet.thumbnails.default,
-              duration: duration
-            };
-          });
-          resolve(items);
-        },
-        function(err) {
-          console.error("Execute error", err);
-          resolve([]);
-        }
-      );
+  const response = await gapi.client.youtube.videos.list({
+    part: ["contentDetails,snippet"],
+    id: videoIds
+  });
+  console.log("Response - videos", response);
+  return response.result.items.map(piece => {
+    const duration = toSeconds(parse(piece.contentDetails.duration));
+    return {
+      id: piece.id,
+      title: piece.snippet.title,
+      avatar: piece.snippet.thumbnails.default,
+      duration: duration
+    };
   });
 };
-const getPlaylists = gapi => {
-  return new Promise((resolve, reject) => {
-    gapi.client.youtube.playlists
-      .list({
-        part: ["snippet,contentDetails"],
-        mine: true
-      })
-      .then(
-        function(response) {
-          console.log("Response - user playlists", response);
-          const items = response.result.items.map(playlist => {
-            return {
-              id: playlist.id,
-              title: playlist.snippet.title,
-              avatar: playlist.snippet.thumbnails.default
-            };
-          });
-          resolve(items);
-        },
-        function(err) {
-          console.error("Execute error", err);
-          resolve([]);
-        }
-      );
+const getPlaylists = async gapi => {
+  const response = await gapi.client.youtube.playlists.list({
+    part: ["snippet,contentDetails"],
+    mine: true
+  });
+  console.log("Response - user playlists", response);
+  return response.result.items.map(playlist => {
+    return {
+      id: playlist.id,
+      title: playlist.snippet.title,
+      avatar: playlist.snippet.thumbnails.default
+    };
   });
 };
 
-const getPlaylistItems = (gapi, playlistId, gameMode) => {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    gapi.client.youtube.playlistItems
-      .list({
-        part: ["id,status,snippet,contentDetails"],
-        playlistId: playlistId
-      })
-      .then(
-        async function(response) {
-          console.log("Response - playlist items", response);
-          let items = response.result.items.map(piece => {
-            return {
-              id: piece.contentDetails.videoId,
-              title: piece.snippet.title,
-              avatar: piece.snippet.thumbnails.default
-            };
-          });
-          if (gameMode === "tournament") {
-            items = await getVideosWithDurations(gapi, items);
-          }
-          resolve(items);
-        },
-        function(err) {
-          console.error("Execute error", err);
-          resolve([]);
-        }
-      );
+const getPlaylistItems = async (gapi, playlistId, gameMode) => {
+  const response = await gapi.client.youtube.playlistItems.list({
+    part: ["id,status,snippet,contentDetails"],
+    playlistId: playlistId
   });
+  console.log("Response - playlist items", response);
+  let items = response.result.items.map(piece => {
+    return {
+      id: piece.contentDetails.videoId,
+      title: piece.snippet.title,
+      avatar: piece.snippet.thumbnails.default
+    };
+  });
+  if (gameMode === "tournament") {
+    items = await getVideosWithDurations(gapi, items);
+  }
+  return items;
 };
 
 export { getPlaylists, getPlaylistItems };

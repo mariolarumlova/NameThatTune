75aa3f87c3eb08728b0abdfe5b90dd4faa69ca03
Mariola Rumlova <mariolarumlova@gmail.com>
2021-04-11 22:18:10 +0200

    Added wiki at home page

--- name-that-tune/src/components/utils/ChooseGameMode.vue
+++ name-that-tune/src/components/utils/ChooseGameMode.vue
@@ -3,14 +3,23 @@
     <v-row align="center" justify="space-around">
       <v-col align="center" justify="space-around">
         <div class="text-h4 pa-4">
-          Hello, {{ clientName }}! Choose game mode <br />
+          Hello, {{ clientName }}! <br />
+          Choose game mode <br />
+          <v-btn class="ma-8" elevation="2" outlined large to="training"
+            >Training</v-btn
+          >
+          <v-btn class="ma-8" elevation="2" outlined large to="tournament"
+            >Tournament</v-btn
+          >
+          <v-btn
+            class="ma-8"
+            elevation="2"
+            outlined
+            large
+            href="https://github.com/mariolarumlova/NameThatTune/wiki"
+            ><v-icon>mdi-help-circle</v-icon>Read the tutorial</v-btn
+          >
         </div>
-        <v-btn class="ma-8" elevation="2" outlined large to="training"
-          >Training</v-btn
-        >
-        <v-btn class="ma-8" elevation="2" outlined large to="tournament"
-          >Tournament</v-btn
-        >
       </v-col>
     </v-row>
   </v-container>
@@ -18,24 +27,16 @@
 
 <script>
 import { mapGetters } from "vuex";
-import databaseFactory from "@/dataProvider/classes/Database";
-import settingsFactory from "@/dataProvider/dto/Settings";
 
 export default {
   data() {
     return {
       clientName: this.$store.state.session.user.displayName,
-      clientUid: this.$store.state.uid,
-      users: ""
+      clientUid: this.$store.state.uid
     };
   },
   computed: {
     ...mapGetters(["session"])
-  },
-  created: async function() {
-    const db = databaseFactory();
-    const factory = settingsFactory(db);
-    this.users = JSON.stringify(await factory.getAll());
   }
 };
 </script>
